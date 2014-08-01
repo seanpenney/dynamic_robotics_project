45210e6a957ae6dca63a181ae152ede471a220a9
power10dan <lintzu@onid.oregonstate.edu>
2014-07-31 17:11:55 -0700

    Initial Commit of Function To Get Data From Model Blocks

---
+++ Simulink GUI/GUI_Functions/GetData.m
@@ -0,0 +1,34 @@
+% function data_gotten = GetData
+% 
+% Description:
+%   
+% Gets all of the data from the Simulink Model for GUI display 
+%  
+% Fields: None
+% 
+% Initial: None
+% 
+% Final: None 
+%
+
+function data_gotten = GetData(model)
+
+    Model_Initialization_Check(model);
+    
+    % get initial data states of the model 
+        
+  
+    
+
+
+
+
+
+
+    % return the data as a struct 
+
+
+
+
+
+end
