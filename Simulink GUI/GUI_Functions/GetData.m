ee7a26391b2c3297426a31d8bb4d3ebb89758843
power10dan <lintzu@onid.oregonstate.edu>
2014-08-01 17:05:57 -0700

    Progress on Making Function Take Data For Simulink GUI

--- Simulink GUI/GUI_Functions/GetData.m
+++ Simulink GUI/GUI_Functions/GetData.m
@@ -4,27 +4,52 @@
 %   
 % Gets all of the data from the Simulink Model for GUI display 
 %  
-% Fields: None
+% Fields: model
 % 
-% Initial: None
+% Initial: model must be a string
 % 
-% Final: None 
+% Final: outputs a struct with parameters model name(s) and its value(s)
 %
 
-function data_gotten = GetData(model)
+%% Function Not Complete
+function block_data = GetData(model)
 
     Model_Initialization_Check(model);
     
-    % get initial data states of the model 
-        
-  
+    % Get all blocks in model 
+    BlockPaths = find_system('torquecontrol','Type','Block');
+     
+    % if the model is not running, get the initial value and return
     
-
-
-
-
-
-
+    z = get_param('model_name','SimulationStatus');
+    
+    if strcmp(z, 'stopped') == 1
+        
+        % get block value parameter
+        for idx = 1:numel(BlockPaths)
+        
+            
+            
+            
+                                   
+        
+        end
+    
+    end
+    
+    
+    
+    % get all values in the block during run time
+     
+    for idx = 1:numel(BlockPaths)
+        
+        % get values from each of the blocks
+        
+         
+         
+         
+    end
+  
     % return the data as a struct 
 
 
