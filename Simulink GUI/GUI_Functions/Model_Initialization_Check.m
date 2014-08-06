120bc2c05d1d6980b67b028de279a054fb9b06fd
power10dan <lintzu@onid.oregonstate.edu>
2014-08-05 17:07:38 -0700

    Minor Modifications To Add Functionalities

--- Simulink GUI/GUI_Functions/Model_Initialization_Check.m
+++ Simulink GUI/GUI_Functions/Model_Initialization_Check.m
@@ -2,7 +2,7 @@
 % 
 % Description:
 %  
-% This function checks whether the Simulink Model model_name is opened. 
+% This function checks whether the Simulink Model model_name is opened or loaded. 
 % If not, the function warns the user and let the user open the model_name
 %  
 % Fields: model_name
@@ -11,23 +11,38 @@
 % 
 % Final: none 
 %
-function Model_Initialization_Check(model_name)
+function status = Model_Initialization_Check(model_name)
 
-    % Get all the currently opened models 
+    % get all the currently opened models 
     opened_models = find_system('SearchDepth',0);
     
-    model_index = find(ismember(opened_models,'torquecontrol'));
+    % check if model is open
+    model_index = find(ismember(opened_models, model_name));
+    
+    % check if model is loaded
+    isLoaded = bdIsLoaded(model_name);
     
     if isempty(model_index)
         
-        model_error_message = sprintf('Your %s is not opened, please open it!', model_name);
-        message_box_handle = msgbox(model_error_message);
+        if isLoaded == 0
         
-        if message_box_handle == 1 % wait until user click ok on box handle
-        
-            OpenModel;
-        
+            model_error_message = sprintf('Your %s is not loaded, please select the model you want to load!', model_name);
+            message_box_handle = msgbox(model_error_message);
+
+            if message_box_handle == 1 % wait until user click ok on box handle
+
+                OpenModel;
+                status = 0;
+
+            end
+
+        else 
+            
+            status = 1;
+            return
+         
         end
+            
    
     end
   
