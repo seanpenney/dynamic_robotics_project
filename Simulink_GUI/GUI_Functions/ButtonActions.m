594f40f0338ca790fc750110dad2ca94a5579c39
pclary <lintzu@onid.oregonstate.edu>
2014-08-20 17:37:24 -0700

    Estop, Stop, Start, And Reset Buttons Working

--- Simulink_GUI/GUI_Functions/ButtonActions.m
+++ Simulink_GUI/GUI_Functions/ButtonActions.m
@@ -1,49 +1,46 @@
 % function ButtonActions( input_args )
-% 
+%
 % Description:
-% 
-%  This function handles all button functionalities  
-% 
+%
+%  This function handles all button functionalities
+%
 % Fields: button_type
-% 
+%
 % Initial: button_type is a type string
-% 
+%
 % Final: none
 %
 
 
 function ButtonActions(button_type)
 
+switch button_type
     
-    switch button_type
-
-        case 'Stop'
-
-           assignin('base', 'gui_disable_cmd', 1);
-           
-          
-
-        case 'EStop'
-            
-            
-            %TODO
-            
-        case  'Go'
-            
-             assignin('base', 'gui_enable_cmd', 1);
-           
-      
-        case  'Restart'
+    case 'Stop'
+        set_cmd('gui_disable_cmd');
+        
+    case 'EStop'
+        set_cmd('gui_estop_cmd');
  
-             assignin('base', 'gui_reset_cmd', 1);
-           
+    case  'Go'
+        set_cmd('gui_enable_cmd');
+        
+    case  'Restart'
+        set_cmd('gui_reset_cmd');
+        
+    otherwise
+        disp('Something went wrong');
+        
+        
+end
+end
 
-        otherwise
 
-            disp('Something went wrong');
+function set_cmd(cmd)
 
-
-    end
+assignin('base', cmd, 1);
+set_param('atrias_system', 'SimulationCommand', 'Update');
+assignin('base', cmd, 0);
 
 end
 
