3badf907d2616f0b1850bd0b15bafbbf5b0807c5
seanpenney <seanjpenney@gmail.com>
2014-10-23 15:23:11 -0700

    cleaned up drl repo

--- simulink_models/9-17-14 before reverting/atrias-common/scripts/set_eni_loc_v2.m
+++
@@ -1,2 +0,0 @@
-% Set the proper path for the EtherCAT Master block's ENI file in the DAQ block
-set_param('atrias_system/EtherCAT Init', 'config_file', [fileparts(mfilename('fullpath')) '/../models/atrias_with_boom_and_imu.xml'])
